106cca9374c5f657e6f0fea4ed1fa59f370f1817
Masonk7 <150885585+Masonk7@users.noreply.github.com>
2025-12-02 14:48:23 -0600

    code to get flask application running

--- app.py
+++ app.py
@@ -4,3 +4,10 @@ from sqlalchemy.sql import func
 import os
 import uuid
 
+app = Flask(__name__)
+app.config["SECRET_KEY"] = "root"  
+BASE_DIR = os.path.abspath(os.path.dirname(__file__))
+db_path = os.path.join(BASE_DIR, "reservations.db")
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_path}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
